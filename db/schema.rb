f5b1a3a37445c1b333b314442ce5e394c270db8b
Rifqi Ardian <rifardian@gmail.com>
2017-11-30 07:36:26 +0700

    initial commit branch development

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171126122123) do
+ActiveRecord::Schema.define(version: 20171127205955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,15 @@ ActiveRecord::Schema.define(version: 20171126122123) do
     t.string "no_telepon"
     t.string "keterangan"
     t.string "profile_picture"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "encrypted_password"
+    t.string "salt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
