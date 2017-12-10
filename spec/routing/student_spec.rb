5cbb91cb2fe214e18b7defd6aea4147bd37b14ce
Rifqi Ardian <rifardian@gmail.com>
2017-12-10 07:11:26 +0700

    fix ci

--- spec/routing/student_spec.rb
+++ spec/routing/student_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "display login when route to student_path before login" do
 	it "Succesfully visit the homepage" do
-		visit 'http://0.0.0.0:3000/students'
+		visit 'http://0.0.0.0:3000/users'
 		expect(current_path).to eq(new_user_session_path)
 	end
 end
