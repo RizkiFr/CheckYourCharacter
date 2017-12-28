304df476bec887a91fba9a32fd3f902bdd8d9938
Rifqi Ardian <rifardian@gmail.com>
2017-12-28 21:54:13 +0700

    fix my post to info psikologi

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,51 +1,7 @@
 class PostsController < ApplicationController
+	before_action :authenticate_user!
 	def index
 		@user = current_user.id
 		@posts = Article.where(:user_id => @user)
 	end
-
-	def new
-    @post = Article.new
-  end
-
-  def edit
-    @post = Article.find(params[:id])
-  end
-
-  def show
-    @user=current_user
-    @post = Article.find(params[:id])
-  end
-
-  def create
-    @post = Article.new(post_params)
-
-    if @post.save
-      redirect_to posts_path, notice: 'Data berhasil di simpan'
-    else
-      render 'new'
-    end
-  end
-
-  def update
-    @post = Article.find(params[:id])
-
-    if @post.update(post_params)
-      redirect_to posts_path, notice: 'Data berhasil di ubah'
-    else
-      render 'edit'
-    end
-  end
-
-  def destroy
-    @post = Article.find(params[:id])
-    @post.destroy
-
-    redirect_to posts_path
-  end
-
-  private
-    def post_params
-      params.require(:article).permit(:title, :text, :user_id)
-    end
 end
