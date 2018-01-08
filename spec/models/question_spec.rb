02e4b8c2074fd557fbf181e87c21761084263143
Rifqi Ardian <rifardian@gmail.com>
2018-01-09 06:14:42 +0700

    add comment & complaint testing

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,8 +2,16 @@ require "rails_helper"
 
 RSpec.describe Question, type: :model do
   #it 'should save succesfully' do
-   #   question = Question.new(no:1, dominance:'adf',influence: 'asd', steadiness: 'jk', compliance: 'ls' ).save
-    #  expect(question).to eq(true)
+   	#question = Question.create!(no: "1") 
+    #expect(question.no).to eq("1")
+    #question = Question.create!(dominance: "a") 
+    #expect(question.dominance).to eq("a")
+    #question = Question.create!(influence: "b") 
+    #expect(question.influence).to eq("c")
+    #question = Question.create!(steadiness: "c") 
+    #expect(question.steadiness).to eq("c")
+    #question = Question.create!(compliance: "d") 
+    #expect(question.compliance).to eq("d") 
   #end
   it 'should failed to save because without no' do
       question = Question.new( dominance:'adf',influence: 'asd', steadiness: 'jk', compliance: 'ls' ).save
